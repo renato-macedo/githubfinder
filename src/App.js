c1b52bbfe97eefb146135b03e038814f29b12e32
RenatoSpll <renatomacedo500@gmail.com>
2019-07-08 23:48:24 -0300

    starting course, jsx, components, props, propsTypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: false,
+  }
+  
+  load = () => {
+    this.setState({ loading: true }, () => {
+      setTimeout(() => {
+        this.setState({ loading: false })
+      },1000)
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        { this.state.loading ? <h4>Loading...</h4> : 
+        <div>
+          <h1>Loaded</h1>
+        <button onClick={this.load}>load</button>
+        </div> 
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
